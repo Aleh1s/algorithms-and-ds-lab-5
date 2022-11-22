1446e6b60f64dfac314cc388cd2dc8a5002ee547
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-11-22 19:27:47 +0200

    add some constants, update .gitignore

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -1,11 +1,14 @@
 package org.example;
 
 public class Constants {
-    public static final String XSD_FILE_NAME = "src/main/resources/graph.xsd";
-    public static final String APP_PROPS_FILE_NAME = "src/main/resources/app.properties";
+    public static final String P = "p";
     public static final String ALFA = "alfa";
     public static final String BETA = "beta";
     public static final String L_MIN = "lMin";
+    public static final String ITERATIONS = "iterations";
     public static final String NUMBER_OF_ANTS = "number.of.ants";
-    public static final String GRAPH_SOURCE_FILE_NAME = "src/main/resources/graph.xml";
+    public static final String OUTPUT_FILE_NAME = "output.file.name";
+    public static final String XSD_FILE_NAME = "src/main/resources/graph.xsd";
+    public static final String APP_PROPS_FILE_NAME = "src/main/resources/app.properties";
+    public static final String GRAPH_SOURCE_FILE_NAME = "src/main/resources/15valueInput.xml";
 }
