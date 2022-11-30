a9deaadaaaa03ab98c35c00d5b8540e6c6646f0d
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-11-30 19:21:43 +0200

    add statistic class

---
+++ src/main/java/org/example/Statistics.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Statistics {
+    private final List<Double> extraPheromone = new LinkedList<>();
+    private final List<Integer> distances = new LinkedList<>();
+    private final List<Double> wishes = new LinkedList<>();
+
+    public void addExtraPheromone(double extraPheromone) {
+        this.extraPheromone.add(extraPheromone);
+    }
+
+    public void addDistances(List<Integer> distances) {
+        this.distances.addAll(distances);
+    }
+
+    public void addWish(double wish) {
+        this.wishes.add(wish);
+    }
+
+    private double getAverageExtraPheromone() {
+        return extraPheromone.stream().mapToDouble(e -> e).average().orElse(0);
+    }
+
+    private double getAverageWish() {
+        return wishes.stream().mapToDouble(w -> w).average().orElse(0);
+    }
+
+    private double getAverageDistance() {
+        return distances.stream().mapToInt(d -> d).average().orElse(0);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Statistics{");
+        sb.append("averageExtraPheromone=").append(getAverageExtraPheromone());
+        sb.append(", averageDistance=").append(getAverageDistance());
+        sb.append(", averageWish=").append(getAverageWish());
+        sb.append('}');
+        return sb.toString();
+    }
+}
